99102355aaeb6d13048b1a4d32ea179743d240f2
An Yonghee <ayh0429@gmail.com>
2022-08-29 22:53:44 +0900

    edited VideoUploadPage

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,50 +1,145 @@
-import React from 'react'
-import  {Typogrphy, Button, Form, message, Input, Icon } from 'antd';
-import TextArea from 'antd/lib/input/TextArea';
+import React from "react"
 
-const { TextArea } = Input;
-const { Title } = Typogrphy;
+import { Typography, Button, Form, message, Input, Icon } from 'antd'
 
-function ViedoUploadPage() {
-  return (
-    <div style={{ maxwidth: '700px', margin: '2rem auto'}}>
-      <div style={{ textAlign: 'center', marginBottom: '2rem'}}>
-        <Title level={2}>Upload Video</Title>
-      </div>
-      <Form onSubmit>
-        <div style={{display:'flex', justifyContent:'space-between'}}>
-          
-          <div>
-            <img src alt />
-          </div>
+import Dropzone from 'react-dropzone'
+
+
+
+const { TextArea } = Input
+
+const { Title } = Typography
+
+
+
+function VideoUploadPage() {
+
+    return (
+
+        <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+
+            <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+
+                <Title level={2}>Upload Video</Title>
+
+            </div>
+
+
+
+            <Form onSubmit>
+
+                <div style={{ display: 'flex', justifyContent: 'space-between'}}>
+
+                    {/* Drop zone */}
+
+
+
+                    <Dropzone
+
+                    onDrop
+
+                    multiple
+
+                    maxSize
+
+                    >
+
+                    {({ getRootProps, getInputProps}) => (
+
+                        <div style={{ width: '300px', height: '240px', border: '1px solid lightgray', display: 'flex',
+
+                        alignItems: 'center', justifyContent: 'center'}} {...getRootProps()}>
+
+                            <input {...getInputProps()} />
+
+                            <Icon type='plus' style={{ fontSize: '3rem'}}/>
+
+                        </div>
+
+                    )}
+
+                    </Dropzone>
+
+
+
+                    {/* Thumbnail */}
+
+                    <div>
+
+                        <img src alt />
+
+                    </div>
+
+                </div>
+
+               
+
+                <br/>
+
+                <br/>
+
+                <label>Title</label>
+
+                <Input
+
+                    onChange
+
+                    value
+
+                />
+
+                <br/>
+
+                <br/>
+
+                <label>Description</label>
+
+                <TextArea
+
+                    onChange
+
+                    value
+
+                />
+
+                <br/>
+
+                <br/>
+
+                <select onChange>
+
+                    <option key value></option>
+
+                </select>
+
+                <br/>
+
+                <br/>
+
+                <select onChange>
+
+                    <option key value></option>
+
+                </select>
+
+                <br/>
+
+                <br/>
+
+                <Button type="primary" size="large" onClick>
+
+                    Submit
+
+                </Button>
+
+            </Form>
+
         </div>
-        <br />
-        <br />
-        <label>Title</label>
-      <input
-        onChange
-        value
-      /> 
-      <br />
-      <br />
-      <label>Description</label> 
-      <TextArea
-        onChange
-        value
-      />
-      </Form>
-      <br />
-      <br />
 
-      <select onChange>
-        <option key value></option>
-      </select>
+    )
 
-      <Button type='primary' size='large' onClick >
-
-      </Button>
-    </div>
-  )
 }
 
-export default ViedoUploadPageP
+
+
+export default VideoUploadPage
